5d53f11a499d5f7bb2e72f992bd4b93b8b301fb0
Juliano Rafael <juliano@getty.io>
2018-05-16 16:47:53 -0300

    :building_construction: Setup DesignSytem helper with spacing and colors

---
+++ components/DesignSystem.js
@@ -0,0 +1,16 @@
+export const Spacing = {
+  small: 4,
+  regular: 8,
+  medium: 16,
+  big: 24,
+  large: 32
+}
+
+export const Colors = {
+  background: '#191A2A',
+  darkerBackground: '#10101A',
+  primaryText: '#FFFFFF',
+  secondaryText: '#66688F',
+  primaryGradient: ['#FF4465', '#F6CA1D'],
+  yellow: '#F5FF30'
+}
