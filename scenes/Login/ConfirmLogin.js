0b0721e6cb9f65049733e9d8a88eef8d3dab12bb
Bertao <pedrobertao11@hotmail.com>
2018-05-28 10:48:58 -0300

    fixing pk

--- scenes/Login/ConfirmLogin.js
+++ scenes/Login/ConfirmLogin.js
@@ -78,6 +78,7 @@ class ConfirmLogin extends Component {
     try {
       totpCode ? await Auth.verifyTotpToken(user, code) : await Auth.confirmSignIn(user, code, 'SOFTWARE_TOKEN_MFA')
 
+      const userPublicKey = await Client.getPublicKey();
       if (userPublicKey) {
         this.setState({
           loadingConfirm: false,
