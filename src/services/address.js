d3c15f29e6e110dd0b827c5582079e5f344897d4
Bertao <pedrobertao11@hotmail.com>
2018-05-31 20:06:28 -0300

    address update

--- src/services/address.js
+++ src/services/address.js
@@ -1,9 +1,10 @@
-const ADDRESS_PREFIX = '27'
-const ADDRESS_SIZE = 35
+const ADDRESS_PREFIX = 'T';
+const ADDRESS_SIZE = 34;
 
 export const isAddressValid = address => {
-  if (!address || !address.length) return false
-  if (address.length !== ADDRESS_SIZE) return false
-  if (address.substr(0, 2).toUpperCase() !== ADDRESS_PREFIX.toUpperCase()) return false
-  return true
-}
+    if (!address || !address.length) return false;
+    if (address.length !== ADDRESS_SIZE) return false;
+    //  if (address.substr(0, 1).toUpperCase() !== ADDRESS_PREFIX.toUpperCase()) return false;
+    return address.match(/([A-Za-z0-9]){34}/g);
+
+};
