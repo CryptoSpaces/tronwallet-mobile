c833cd28c9fd6b89f9a3aa113569cad29138ee98
dbuarque <dio@getty.io>
2018-06-02 12:03:26 -0300

    update aws-config

--- aws-exports.js
+++ aws-exports.js
@@ -1,12 +1,20 @@
-const awsmobile = {
-  aws_project_name: 'TronWallet',
-  aws_project_region: 'us-east-1',
-  aws_cognito_identity_pool_id: 'us-east-1:c5a0a18d-8fe2-421b-858d-e6a2c7f97f0a',
-  aws_cognito_region: 'us-east-1',
-  aws_user_pools_id: 'us-east-1_i3fDuXSwH',
-  aws_user_pools_web_client_id: '3rj2t1v5d9cj2jlrk9u6sq4i2s',
-  aws_mobile_analytics_app_id: 'fba1fe8b67264f0a9d1b352bfa6d1458',
-  aws_mobile_analytics_app_region: 'us-east-1'
-}
+const awsmobile = {const awsmobile = {
+    aws_app_analytics: 'enable',
+    aws_project_name: 'TronWallet',
+    aws_project_region: 'us-east-1',
+    aws_content_delivery: 'enable',
+    aws_content_delivery_bucket: 'tronwallet-hosting-mobilehub-1368226614',
+    aws_content_delivery_bucket_region: 'us-east-1',
+    aws_cognito_identity_pool_id: 'us-east-1:c5a0a18d-8fe2-421b-858d-e6a2c7f97f0a',
+    aws_cognito_region: 'us-east-1',
+    aws_content_delivery_cloudfront: 'enable',
+    aws_content_delivery_cloudfront_domain: 'd3s2vx6wuhj1t2.cloudfront.net',
+    aws_user_pools_id: 'us-east-1_i3fDuXSwH',
+    aws_user_pools_web_client_id: '6qpre48ps6uuhjjousm62qp99c',
+    aws_mobile_analytics_app_id: '4cf542cb098945f484dc7ba4be2a4466',
+    aws_mobile_analytics_app_region: 'us-east-1',
+    aws_project_id: 'e8b9c5b9-a46e-4ead-9a65-6f408edd342d',
+    aws_resource_name_prefix: 'tronwallet-mobilehub-1368226614'
+ };
 
-export default awsmobile
+export default awsmobile;
