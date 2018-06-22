e09bc8b0714992760eef30089e24d7c684045ddc
Juliano Rafael <juliano@getty.io>
2018-06-22 14:00:17 -0300

    :heavy_plus_sign: Linking camera, qrcode-scanner and permissions

--- android/app/src/main/java/com/tronwallet2/MainApplication.java
+++ android/app/src/main/java/com/tronwallet2/MainApplication.java
@@ -3,6 +3,7 @@ package com.tronwallet2;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.amazonaws.RNAWSCognitoPackage;
 import io.realm.react.RealmReactPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
             new SplashScreenReactPackage(),
             new RNAWSCognitoPackage(),
             new RealmReactPackage(),
