53811645c7150de3578ae257222ef0751fef1575
dbuarque <dio@getty.io>
2018-07-06 20:27:26 -0300

    fix release android

--- android/app/src/main/java/com/tronwallet2/MainApplication.java
+++ android/app/src/main/java/com/tronwallet2/MainApplication.java
@@ -2,6 +2,7 @@ package com.tronwallet2;
 
 import android.app.Application;
 import com.facebook.react.ReactApplication;
+import com.amazonaws.RNAWSCognitoPackage;
 import io.sentry.RNSentryPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import io.getty.rntron.RNTronPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNAWSCognitoPackage(),
             new RNSentryPackage(),
             new RNDeviceInfo(),
             new RNTronPackage(),
