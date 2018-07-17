1db58b5b7f4cd76831fc6fdc918f7b09d7c7e621
Carlos P Navarrete Filho <karnav@gmail.com>
2018-07-17 14:18:18 -0300

    adapt Badge to use guarantee flag

--- src/components/Badge/index.js
+++ src/components/Badge/index.js
@@ -1,27 +1,45 @@
 import React from 'react'
 import styled from 'styled-components'
+import * as Utils from '../../components/Utils'
 
 const Wrapper = styled.View`
   background-color: ${props => props.bg};
-  border-radius: 4px;
-  padding: 8px;
+  border-radius: 2px;
+  paddingVertical: 7px;
+  paddingHorizontal: 10px;
   shadow-offset: 0 5px;
   shadow-radius: 10px;
   shadow-color: #000000;
   shadow-opacity: 0.1;
+  justify-content: center;
+  position: relative;
+  overflow: visible;
 `
 
 const Text = styled.Text`
+  top: 1px;
   font-size: 13px;
   font-family: Rubik-Medium;
   line-height: 14px;
   color: #FFFFFF;
 `
 
-const Badge = ({ bg = '#2E2F47', children }) => (
-  <Wrapper bg={bg}>
-    {typeof children === 'string' ? <Text>{children}</Text> : children}
-  </Wrapper>
+const Guarantee = styled.Image`
+  width: 15px;
+  height: 15px;
+  z-index: 999;
+  left: -3;
+`
+
+const Badge = ({ bg = '#2E2F47', guarantee, children }) => (
+  <Utils.Row align='center'>
+    <Wrapper bg={bg}>
+      {typeof children === 'string' ? <Text>{children}</Text> : children}
+    </Wrapper>
+    {guarantee && (
+      <Guarantee source={require('../../assets/guarantee.png')} />
+    )}
+  </Utils.Row>
 )
 
 export default Badge
