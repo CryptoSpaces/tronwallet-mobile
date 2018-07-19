5aaf6e7cdd1d78bfca449f6c90c68fddfcba4287
Renan Nascimento <Renan@costanascimento.com.br>
2018-07-19 11:15:37 -0300

    added featured style to card component

--- src/scenes/Participate/Elements.js
+++ src/scenes/Participate/Elements.js
@@ -1,13 +1,22 @@
 import { Colors, FontSize } from '../../components/DesignSystem'
-import { Content, Text } from '../../components/Utils'
+import { Text, View } from '../../components/Utils'
 
-export const Card = Content.extend`
+export const Card = View.extend`
   margin-right: 25;
   margin-left: 25;
   border-radius: 10;
   background: ${Colors.lightestBackground};
+  overflow: hidden;
 `
 export const CardHeader = Text.extend`
   font-size: ${FontSize.medium};
-  font-weight: 700
+  font-weight: 700;
+`
+
+export const Featured = View.extend`
+  width: 30%;
+  margin: 0 auto;
+  background: ${Colors.unconfirmed};
+  border-bottom-left-radius: 5;
+  border-bottom-right-radius: 5;
 `
--- src/scenes/Participate/index.js
+++ src/scenes/Participate/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import { ScrollView, TouchableOpacity } from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons'
+import { ScrollView, TouchableOpacity } from 'react-native'
+import LinearGradient from 'react-native-linear-gradient'
 
 import NavigationHeader from '../../components/Navigation/Header'
-import { Container, Row, VerticalSpacer, View, Text } from '../../components/Utils'
-import { Card, CardHeader } from './Elements'
+
+import {
+  Container,
+  Content,
+  View,
+  Row,
+  VerticalSpacer,
+  Text
+} from '../../components/Utils'
+
+import { Card, CardHeader, Featured } from './Elements'
 import { Colors } from '../../components/DesignSystem'
 
 class ParticipateHome extends React.Component {
@@ -14,7 +24,7 @@ class ParticipateHome extends React.Component {
         <NavigationHeader
           title='PARTICIPATE'
           rightButton={
-            <TouchableOpacity onPress={() => { }}>
+            <TouchableOpacity onPress={() => {}}>
               <Ionicons name='ios-search' color='white' size={21} />
             </TouchableOpacity>
           }
@@ -23,9 +33,14 @@ class ParticipateHome extends React.Component {
     }
   }
 
-  _renderCard = () => {
-    return (
-      <Card>
+  _renderCardContent = isFeatured => (
+    <React.Fragment>
+      {isFeatured && (
+        <Featured>
+          <Text align='center'>FEATURED</Text>
+        </Featured>
+      )}
+      <Content>
         <Row justify='space-between'>
           <CardHeader>TRONCash</CardHeader>
           <CardHeader>0.10 TRX</CardHeader>
@@ -37,7 +52,30 @@ class ParticipateHome extends React.Component {
           <Text>End in 165 days</Text>
           <Text>70%</Text>
         </Row>
-      </Card>
+      </Content>
+    </React.Fragment>
+  )
+
+  _renderCard = isFeatured => {
+    return (
+      <React.Fragment>
+        <TouchableOpacity onPress={() => {}}>
+          <Card>
+            {isFeatured ? (
+              <LinearGradient
+                start={{ x: 0, y: 1 }}
+                end={{ x: 1, y: 0 }}
+                colors={[Colors.buttonGradient[0], Colors.buttonGradient[1]]}
+              >
+                {this._renderCardContent(isFeatured)}
+              </LinearGradient>
+            ) : (
+              this._renderCardContent()
+            )}
+          </Card>
+          <VerticalSpacer size='medium' />
+        </TouchableOpacity>
+      </React.Fragment>
     )
   }
 
@@ -45,6 +83,7 @@ class ParticipateHome extends React.Component {
     return (
       <Container>
         <ScrollView>
+          {this._renderCard(true)}
           {this._renderCard()}
         </ScrollView>
       </Container>
