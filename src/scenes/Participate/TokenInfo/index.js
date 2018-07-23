1021833900cf66e09b4fa40802e8ae5c6a1b9a86
Renan Nascimento <Renan@costanascimento.com.br>
2018-07-23 12:03:47 -0300

    fixed token info design

--- src/scenes/Participate/TokenInfo/index.js
+++ src/scenes/Participate/TokenInfo/index.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react'
-import { ScrollView } from 'react-native'
+import { ScrollView, SafeAreaView } from 'react-native'
 import ProgressBar from 'react-native-progress/Bar'
 import moment from 'moment'
 
@@ -38,61 +38,63 @@ class TokenInfo extends PureComponent {
       block
     } = this.props.navigation.state.params.item
     return (
-      <ScrollView>
-        <Utils.Container justify='flex-start'>
-          <Utils.VerticalSpacer />
-          <Utils.Row justify='center'>
-            <Badge bg={Colors.lightestBackground} large>{name}</Badge>
-          </Utils.Row>
-          <Utils.VerticalSpacer size='big' />
-          <BoldInfoRow pairs={[
-            { key: 'PRICE PER TOKEN', value: `${price / ONE_TRX} TRX` },
-            { key: 'FROZEN', value: `${frozenPercentage}%` }]}
-          />
-          <Utils.Content paddingVertical='small'>
-            <Utils.SectionTitle>PERCENTAGE</Utils.SectionTitle>
-            <Utils.Row align='center'>
-              <ProgressBar
-                progress={Math.round(issuedPercentage) / 100}
-                borderWidth={0}
-                height={3}
-                color={Colors.confirmed}
-                unfilledColor={Colors.lightestBackground}
-                style={{ flex: 1 }}
-              />
-              <Utils.HorizontalSpacer size='medium' />
-              <Utils.BoldText>{Math.round(issuedPercentage)}%</Utils.BoldText>
+      <SafeAreaView style={{ flex: 1, backgroundColor: Colors.background }}>
+        <ScrollView>
+          <Utils.Container justify='flex-start'>
+            <Utils.VerticalSpacer />
+            <Utils.Row justify='center'>
+              <Badge bg={Colors.lightestBackground} large>{name}</Badge>
             </Utils.Row>
-          </Utils.Content>
-          <BoldInfoRow pairs={[
-            { key: 'ISSUED', value: issued },
-            { key: 'TOTAL SUPPLY', value: totalSupply }]}
-          />
-          <Utils.VerticalSpacer size='xsmall' />
-          <SmallRegInfoRow pairs={[
-            { key: 'START TIME', value: moment(startTime).format('DD/MM/YYYY hh:mm A') },
-            { key: 'END TIME', value: moment(endTime).format('DD/MM/YYYY hh:mm A') }]}
-          />
-          <Utils.VerticalSpacer size='small' />
-          <RegularInfoRow pairs={[
-            { key: 'DESCRIPTION', value: description }]}
-          />
-          <Utils.VerticalSpacer size='small' />
-          <RegularInfoRow pairs={[
-            { key: 'TRANSACTION', value: transaction }]}
-          />
-          <Utils.VerticalSpacer size='small' />
-          <RegularInfoRow pairs={[
-            { key: 'OWNER ADDRESS', value: ownerAddress }]}
-          />
-          <Utils.VerticalSpacer size='small' />
-          <BoldInfoRow pairs={[
-            { key: 'TRXNUM', value: trxNum },
-            { key: 'NUM', value: num },
-            { key: 'BLOCK', value: block }]}
-          />
-        </Utils.Container>
-      </ScrollView>
+            <Utils.VerticalSpacer size='big' />
+            <BoldInfoRow pairs={[
+              { key: 'PRICE PER TOKEN', value: `${price / ONE_TRX} TRX` },
+              { key: 'FROZEN', value: `${frozenPercentage}%` }]}
+            />
+            <Utils.Content paddingVertical='small'>
+              <Utils.SectionTitle>PERCENTAGE</Utils.SectionTitle>
+              <Utils.Row align='center'>
+                <ProgressBar
+                  progress={Math.round(issuedPercentage) / 100}
+                  borderWidth={0}
+                  width={250}
+                  height={3}
+                  color={Colors.confirmed}
+                  unfilledColor={Colors.lightestBackground}
+                />
+                <Utils.HorizontalSpacer size='medium' />
+                <Utils.BoldText>{Math.round(issuedPercentage)}%</Utils.BoldText>
+              </Utils.Row>
+            </Utils.Content>
+            <BoldInfoRow pairs={[
+              { key: 'ISSUED', value: issued },
+              { key: 'TOTAL SUPPLY', value: totalSupply }]}
+            />
+            <Utils.VerticalSpacer size='xsmall' />
+            <SmallRegInfoRow pairs={[
+              { key: 'START TIME', value: moment(startTime).format('DD/MM/YYYY hh:mm A') },
+              { key: 'END TIME', value: moment(endTime).format('DD/MM/YYYY hh:mm A') }]}
+            />
+            <Utils.VerticalSpacer size='small' />
+            <RegularInfoRow pairs={[
+              { key: 'DESCRIPTION', value: description }]}
+            />
+            <Utils.VerticalSpacer size='small' />
+            <RegularInfoRow pairs={[
+              { key: 'TRANSACTION', value: transaction }]}
+            />
+            <Utils.VerticalSpacer size='small' />
+            <RegularInfoRow pairs={[
+              { key: 'OWNER ADDRESS', value: ownerAddress }]}
+            />
+            <Utils.VerticalSpacer size='small' />
+            <BoldInfoRow pairs={[
+              { key: 'TRXNUM', value: trxNum },
+              { key: 'NUM', value: num },
+              { key: 'BLOCK', value: block }]}
+            />
+          </Utils.Container>
+        </ScrollView>
+      </SafeAreaView>
     )
   }
 }
