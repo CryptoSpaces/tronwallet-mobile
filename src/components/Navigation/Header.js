528a37b166359b58513c6b41b7eed6aae0cacf44
Renan Nascimento <Renan@costanascimento.com.br>
2018-07-23 15:46:53 -0300

    update Header component to allow search

--- src/components/Navigation/Header.js
+++ src/components/Navigation/Header.js
@@ -7,63 +7,114 @@ import Feather from 'react-native-vector-icons/Feather'
 
 import * as Utils from '../Utils'
 
-const leftElement = (onBack, leftButton) => {
-  let element = null
-
-  if (onBack && !leftButton) {
-    element = <TouchableOpacity onPress={onBack}>
-      <Ionicons
-        name='ios-arrow-round-back'
-        size={32}
-        color={Colors.primaryText}
-      />
-    </TouchableOpacity>
-  } else {
-    element = leftButton
-  }
-  return <Utils.View margin={10} position='absolute' left={10}>
-    {element}
-  </Utils.View>
-}
-
-const rightElement = (onClose, rightButton) => {
-  let element = null
-  if (onClose && !rightButton) {
-    element = <TouchableOpacity onPress={onClose}>
-      <Feather name='x' color='white' size={28} />
-    </TouchableOpacity>
-  } else {
-    element = rightButton
-  }
-  return <Utils.View margin={10} position='absolute' right={10}>
-    {element}
-  </Utils.View>
-}
-
-const NavigationHeader = ({ title, onClose, noBorder, rightButton, onBack, leftButton }) => {
+class NavigationHeader extends React.Component {
   /*
-    onClose = Right Button with X
-    onBack = Left Button with <
-  */
+     onClose = Right Button with X
+     onBack = Left Button with <
+     onSearch = Right Button with magnifying glass
+   */
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    onClose: PropTypes.func,
+    onBack: PropTypes.func,
+    onSearch: PropTypes.func,
+    noBorder: PropTypes.bool,
+    rightButton: PropTypes.element,
+    leftButton: PropTypes.element
+  }
 
-  return (
-    <Utils.Header padding={16} justify='center' noBorder={noBorder}>
-      {leftElement(onBack, leftButton)}
+  state = {
+    isSearching: false
+  }
+
+  _renderLeftElement = (onBack, leftButton) => {
+    let element = null
+
+    if (onBack && !leftButton) {
+      element = <TouchableOpacity onPress={onBack}>
+        <Ionicons
+          name='ios-arrow-round-back'
+          size={32}
+          color={Colors.primaryText}
+        />
+      </TouchableOpacity>
+    } else {
+      element = leftButton
+    }
+    return <Utils.View margin={10} position='absolute' left={10}>
+      {element}
+    </Utils.View>
+  }
+
+  _renderRightElement = (onClose, onSearch, rightButton) => {
+    let element = null
+    if (onClose && !rightButton) {
+      element = <TouchableOpacity onPress={onClose}>
+        <Feather name='x' color='white' size={28} />
+      </TouchableOpacity>
+    } else if (onSearch && !rightButton) {
+      element = <TouchableOpacity onPress={() => { this.setState({ isSearching: true }) }}>
+        <Ionicons name='ios-search' color='white' size={21} />
+      </TouchableOpacity>
+    } else {
+      element = rightButton
+    }
+    return <Utils.View margin={10} position='absolute' right={10}>
+      {element}
+    </Utils.View>
+  }
+
+  _renderDefaultMode = () => {
+    const { title, onClose, rightButton, onBack, leftButton, onSearch } = this.props
+
+    return (
+      <React.Fragment>
+        {this._renderLeftElement(onBack, leftButton)}
+        <Utils.View justify='center' align='center' >
+          <Utils.Text lineHeight={36} size='average' font='medium'>{title.toUpperCase()}</Utils.Text>
+        </Utils.View >
+        {this._renderRightElement(onClose, onSearch, rightButton)}
+      </React.Fragment>
+    )
+  }
+
+  _renderSeachMode = () => {
+    const { onSearch } = this.props
+
+    const onClose = () => { this.setState({ isSearching: false }) }
+    const searchBar = (
       <Utils.View justify='center' align='center'>
-        <Utils.Text lineHeight={36} size='average' font='medium'>{title.toUpperCase()}</Utils.Text>
+        <Utils.FormInput
+          autoCapitalize='none'
+          autoCorrect={false}
+          underlineColorAndroid='transparent'
+          onChangeText={text => onSearch(text)}
+          placeholder='Search'
+          placeholderTextColor='#fff'
+          width={281}
+          height={42}
+        />
       </Utils.View>
-      {rightElement(onClose, rightButton)}
-    </Utils.Header>
-  )
-}
+    )
 
-NavigationHeader.propTypes = {
-  title: PropTypes.string.isRequired,
-  onClose: PropTypes.func,
-  noBorder: PropTypes.bool,
-  rightButton: PropTypes.element,
-  onBack: PropTypes.func,
-  leftButton: PropTypes.element
+    return (
+      <React.Fragment>
+        {this._renderLeftElement(null, searchBar)}
+        {this._renderRightElement(onClose)}
+      </React.Fragment>
+    )
+  }
+
+  render () {
+    const { noBorder } = this.props
+    const { isSearching } = this.state
+
+    return (
+      <Utils.Header padding={16} justify='center' noBorder={noBorder}>
+        {isSearching ? this._renderSeachMode() : this._renderDefaultMode()}
+      </Utils.Header>
+    )
+  }
 }
 
 export default NavigationHeader
