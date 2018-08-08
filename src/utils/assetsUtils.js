51579e10f9300f435b643daf67519ef030761a3f
Carlos Navarrete <karnav@gmail.com>
2018-08-08 10:30:03 -0300

    add crypto chain to verified (#263)

--- src/utils/assetsUtils.js
+++ src/utils/assetsUtils.js
@@ -9,3 +9,32 @@ export const updateAssets = async (start = 0, limit = 100, name = '') => {
 
   return assets
 }
+
+export const orderAssets = (assets) => {
+  let orderedVerified = []
+  let rest = []
+  const verifyAsset = (index, asset) => {
+    asset.verified = true
+    orderedVerified[index] = asset
+  }
+  assets.forEach((asset) => {
+    switch (asset.name) {
+      case 'TRX':
+        verifyAsset(0, asset)
+        break
+      case 'TWX':
+        verifyAsset(1, asset)
+        break
+      case 'CryptoChain':
+        verifyAsset(2, asset)
+        break
+      default:
+        rest.push(asset)
+    }
+  })
+
+  return [
+    ...orderedVerified.filter((asset) => asset),
+    ...rest
+  ]
+}
