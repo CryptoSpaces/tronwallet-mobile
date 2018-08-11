b9fc4f4c27e625aa47d29843a4e406de822ba74d
CryptoSpaces <cryptospaces1@gmail.com>
2018-08-11 16:43:43 +0200

    Added translations to 'momentUtils'.

--- src/utils/momentUtils.js
+++ src/utils/momentUtils.js
@@ -14,6 +14,22 @@ const relativeTime = {
     MM: '%d meses',
     y: 'um ano',
     yy: '%d anos'
+  },
+  nl: {
+    future: 'in %s',
+    past: '%s geleden',
+    s: 'een seconde',
+    ss: '%d seconden',
+    m: 'een minuut',
+    mm: '%d minuten',
+    h: 'een uur',
+    hh: '%d uur',
+    d: 'een dag',
+    dd: '%d dagen',
+    M: 'een maand',
+    MM: '%d maanden',
+    y: 'een jaar',
+    yy: '%d jaar'
   }
 }
 
